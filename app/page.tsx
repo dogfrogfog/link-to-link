5a836ea12bd18292cc5f704e6db833bec9da6828
dogfrogfog <maksim.hodasevich@gmail.com>
2023-03-22 20:27:18 +0100

    remove client side code

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,13 @@
-'use client'
-import { useState } from "react";
-
 const InfoBlock = ({ title, text, bgColor, textColor }: { title: string, text: string, bgColor: string, textColor: string }) => (
-  <div className={`max-w-sm overflow-hidden ${bgColor}`}>
+  <div className={`overflow-hidden ${bgColor}`}>
     {/* <img className="w-full" src="/img/card-top.jpg" alt="Sunset in the mountains"> */}
     <div className="px-6 py-10">
       <div className={`font-black text-3xl mb-2 leading-7 ${textColor}`}>{title}</div>
       <p className={`text-xs ${textColor}`}>{text}</p>
     </div>
-    {/* <div className="px-6 pt-4 pb-2">
-      <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-      <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-      <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
-    </div> */}
   </div>
 )
 
-const QuestionsBlock = ({ question, answer } : { question: string, answer: string }) => {
-  const [isOpen, setIsOpen] = useState(false);
-  return (
-    <div className="bg-red-900 rounded-lg p-2 mb-4">
-      <div className="text-fuchsia-200 leading-5 mb-2 font-medium" onClick={() => setIsOpen(!isOpen)}>{question}</div>
-      {isOpen && <div className="text-sm text-fuchsia-200">{answer}</div>}
-    </div>
-  )
-}
-
-
-// add button
 const landingBlocks = [
   {
     title: 'Everything you are. In one, simple link in bio.',
@@ -55,31 +35,20 @@ const landingBlocks = [
   },
 ]
 
-const questions = [
-  {
-    question: 'Why do I need a link in bio tool?',
-    answer: 'Right now, every time you’ve got something new to share, you have to go to every single one of your channels to change the link in each of your bios. It’s time-consuming and complicated – making it so much harder to keep everything up to date.'
-  },
-  {
-    question: 'Is Linktree the original link in bio tool?',
-    answer: 'Right now, every time you’ve got something new to share, you have to go to every single one of your channels to change the link in each of your bios. It’s time-consuming and complicated – making it so much harder to keep everything up to date.'
-  },
-  {
-    question: 'Is Linktree safe to use on all of my social media profiles?',
-    answer: 'Right now, every time you’ve got something new to share, you have to go to every single one of your channels to change the link in each of your bios. It’s time-consuming and complicated – making it so much harder to keep everything up to date.'
-  },
-]
-
 export default function Home() {
   return (
     <>
       <h1 className="text-3xl font-bold">
-        {landingBlocks.map(v => <InfoBlock key={v.title} title={v.title} text={v.text} textColor={v.textColor} bgColor={v.bgColor} />)}
+        {landingBlocks.map(v => 
+          <InfoBlock
+            key={v.title}
+            title={v.title}
+            text={v.text}
+            textColor={v.textColor}
+            bgColor={v.bgColor}
+          />
+        )}
       </h1>
-      <div className="px-4 bg-red-800 px-6 py-10">
-        <div className="text-center font-bold text-2xl text-fuchsia-200 mb-2">Got questions?</div>
-        {questions.map(v => <QuestionsBlock key={v.question} question={v.question} answer={v.answer} />)}
-      </div>
     </>
   )
 }
