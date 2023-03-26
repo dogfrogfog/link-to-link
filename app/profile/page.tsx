86a378cc637fbf7a3bd64b7e1891b7774bfdd6c6
dogfrogfog <maksim.hodasevich@gmail.com>
2023-03-26 14:04:00 +0200

    add ability to delete links

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,44 +1,32 @@
 import { getServerSession } from 'next-auth/next';
 import { authOptions } from '@/pages/api/auth/[...nextauth]';
 import AddLink from './AddLink';
+import DeleteLinkButton from './DeleteContentButton';
 import db, { Content } from '@/db';
-
-const fields = Array(5).fill({
-  link: 'https://example.com',
-  title: 'Example',
-});
 
 export default async function Profile() {
   const data = await getServerSession(authOptions);
-  const userLinks = await db.content.findMany({
+  const userLinks = (await db.content.findMany({
     where: {
       userId: data?.user.id,
     },
     orderBy: {
       createdAt: 'desc',
-    }
-  }) as Content[];
+    },
+  })) as Content[];
 
   return (
     <div>
       {data && <AddLink />}
       <div className="my-4">
-        {userLinks?.map(v => (
-          <div
-            key={v.id}
-            className="w-full bg-red-200 my-4 p-2 rounded-lg"
-          >
+        {userLinks?.map((v) => (
+          <div key={v.id} className="w-full my-4 p-2 rounded-lg bg-gray-50">
             <div className="flex justify-between">
               <div>
                 <p className="font-bold">{v.title}</p>
                 <p>{v.url}</p>
               </div>
-              {data && (
-                <div>
-                  <button className="btn btn-success">edit</button>
-                  <button className="btn btn-error">delete</button>
-                </div>
-              )}
+              <DeleteLinkButton contentId={v.id} />
             </div>
           </div>
         ))}
